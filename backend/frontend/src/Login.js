ea97c7ac06707d6cf45aff958c1a9132036e5c70
arnik <arnikchakraborty50@gmail.com>
2023-10-19 02:23:46 +0530

    resolved instantaneous loading of login screen while already logged in

--- backend/frontend/src/Login.js
+++ backend/frontend/src/Login.js
@@ -8,6 +8,7 @@ import CircularProgress from "@mui/material/CircularProgress";
 import { useEffect } from "react";
 import Logo5 from "./components/Logo5.png";
 import Toast from "./Toast";
+import LoadingPage from "./components/LoadingPage";
 
 const Login = () => {
   const [error, setError] = useState(0);
@@ -15,6 +16,10 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(0);
   const [errorMessage, setErrorMessage] = useState("");
+  const [token, setToken] = useState("");
+
+  console.log("setter");
+
   function errorSetter() {
     setError(0);
     console.log("i am this");
@@ -63,45 +68,54 @@ const Login = () => {
   }
 
   useEffect(() => {
-    let a = localStorage.getItem("_id");
-    if (a) navigate("/home");
+    setToken(localStorage.getItem("token"));
+    if (localStorage.getItem("token")) {
+      navigate("/home");
+    }
+    console.log(token);
   }, []);
 
   return (
-    <div className="register-wrapper">
-      {!error ? <></> : <Toast message={errorMessage} />}
-      <img src={Logo5} className="register-page-logo" />
-      <p className="register-title">Welcome Back !</p>
+    <>
+      {localStorage.getItem("token") ? (
+        <LoadingPage />
+      ) : (
+        <div className="register-wrapper">
+          {!error ? <></> : <Toast message={errorMessage} />}
+          <img src={Logo5} className="register-page-logo" />
+          <p className="register-title">Welcome Back !</p>
 
-      <input
-        placeholder="Email"
-        className="register-input"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        placeholder="Password"
-        type="password"
-        className="register-input"
-        onChange={(e) => setPassword(e.target.value)}
-      />
+          <input
+            placeholder="Email"
+            className="register-input"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            className="register-input"
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-      <div className="register-button" onClick={loginHandler}>
-        {loading ? <CircularProgress size={25} /> : <p>Log in</p>}
-      </div>
+          <div className="register-button" onClick={loginHandler}>
+            {loading ? <CircularProgress size={25} /> : <p>Log in</p>}
+          </div>
 
-      <p className="register-page-link color-c5cfd6">
-        Don't have an account ?{" "}
-        <span className="register-page-text" onClick={linkToRegister}>
-          Sign up
-        </span>
-      </p>
+          <p className="register-page-link color-c5cfd6">
+            Don't have an account ?{" "}
+            <span className="register-page-text" onClick={linkToRegister}>
+              Sign up
+            </span>
+          </p>
 
-      <p className="register-page-text-wrapper color-c5cfd6">
-        By logging in you accept our
-        <span className="register-page-text">Privacy Policy</span>
-        and <span className="register-page-text">Terms of Service</span>.
-      </p>
-    </div>
+          <p className="register-page-text-wrapper color-c5cfd6">
+            By logging in you accept our
+            <span className="register-page-text">Privacy Policy</span>
+            and <span className="register-page-text">Terms of Service</span>.
+          </p>
+        </div>
+      )}
+    </>
   );
 };
 
