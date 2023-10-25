940ba5d1be91c73797c9bb8fde80c21a8545b165
arnik <arnikchakraborty50@gmail.com>
2023-10-25 20:36:43 +0530

    skeletal loader 1

---
+++ backend/frontend/src/skeletal/membersSkeletal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./membersSkeletal.css";
+
+const membersSkeletal = () => {
+  return (
+    <div className="members-skeletal-wrapper">
+      <div className="members-skeletal-circle"></div>
+      <div className="members-skeletal-circle"></div>
+      <div className="members-skeletal-circle"></div>
+    </div>
+  );
+};
+
+export default membersSkeletal;
